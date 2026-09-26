1e1e16b8fa573e738be79ee05b3d7ef8f38e91f9
mythri-io <ravalapudimythri@klh.edu.in>
2026-09-26 14:38:14 +0530

    Update task_queue.h

--- PROJECT/Multithreaded_Linux_Application /src /task_queue.h
+++ PROJECT/Multithreaded_Linux_Application /src /task_queue.h
@@ -1,1 +1,24 @@
+#ifndef TASK_QUEUE_H
+#define TASK_QUEUE_H
 
+#define QUEUE_SIZE 10
+
+typedef struct
+{
+    int task_id;
+    int value;
+} Task;
+
+typedef struct
+{
+    Task tasks[QUEUE_SIZE];
+    int front;
+    int rear;
+    int count;
+} TaskQueue;
+
+void init_queue(TaskQueue *queue);
+void add_task(TaskQueue *queue, Task task);
+Task get_task(TaskQueue *queue);
+
+#endif
